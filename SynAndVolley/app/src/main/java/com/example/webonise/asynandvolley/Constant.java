6a1c1d9836b24cdc470cdc9346747e7d41f4186e
hemant.bonde <hemant.bonde@weboniselab.com>
2015-08-24 20:12:22 +0530

    Initiall Commit AsysTask : Done Volley , Gson : Pending

---
+++ SynAndVolley/app/src/main/java/com/example/webonise/asynandvolley/Constant.java
@@ -0,0 +1,8 @@
+package com.example.webonise.asynandvolley;
+
+/**
+ * Created by webonise on 24/8/15.
+ */
+public class Constant {
+    public static final String URL = "http://api.androidhive.info/contacts";
+}
---
+++ SynAndVolley/app/src/main/java/com/example/webonise/asynandvolley/HomeScreenActivity.java
@@ -0,0 +1,24 @@
+package com.example.webonise.asynandvolley;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class HomeScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+         /*Json response from async task*/
+        new JsonAsyncTask(this).execute();
+    }
+}
